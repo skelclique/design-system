173200be7c1cce5b577c61a747a48cfb4ebebaa7
Vinicius Bortolo <luizviniciusbortolo@gmail.com>
2024-03-05 20:22:44 -0300

    fix: Add subpath to Storybook on production

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -31,6 +31,13 @@ const config: StorybookConfig = {
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
-  `
+  `,
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/design-system/'
+    }
+    
+    return config
+  } 
 };
 export default config;
